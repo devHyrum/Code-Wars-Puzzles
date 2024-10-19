1c2704396877b9b85a584b2af8f237b2b16d6922
holivera159 <holivera0730@gmail.com>
2024-10-19 17:26:17 -0300

    one more day

---
+++ javascript/9. Decode the Morse code/solution.js
@@ -0,0 +1,47 @@
+const MORSE_CODE = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z',
+    '-----': '0',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9'
+};  
+
+decodeMorse = function(morseCode){
+    const decodedWords = morseCode.trim().split('   ').map((word) => {
+        const characters = word.split(' ');
+        return characters.map(char => MORSE_CODE[char] || '').join('');
+    })
+    return decodedWords.join(' ');
+  }
+console.log(decodeMorse('.... . -.--   .--- ..- -.. .'))
